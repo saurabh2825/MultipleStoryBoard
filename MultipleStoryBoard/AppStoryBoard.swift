da3b84f94dd7f0f2526e79c05ad2a623c10d9554
saurabh suman <saurabh.msuman@gmail.com>
2017-10-18 13:03:03 +0530

    Commit

---
+++ MultipleStoryBoard/AppStoryBoard.swift
@@ -0,0 +1,20 @@
+//
+//  AppStoryBoard.swift
+//  MultipleStoryBoard
+//  Created by vmoksha mobility on 16/10/17.
+//  Copyright © 2017 saurabh suman. All rights reserved.
+//
+
+import UIKit
+
+
+
+enum AppStoryBoard : String {
+    case Main, Login, Settings, Home
+
+    var instance : UIStoryboard {
+       
+        return UIStoryboard(name: self.rawValue, bundle: Bundle.main)
+    }
+    
+}
--- MultipleStoryBoard/ViewController.swift
+++ MultipleStoryBoard/ViewController.swift
@@ -20,6 +20,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func LoginButtonAction(_ sender: Any) {
+   
+        let loginStoryBoard = AppStoryBoard.Login.instance
+        let loginController = loginStoryBoard.instantiateViewController(withIdentifier: "LoginVC")
+        self.present(loginController, animated: true, completion: nil)
+
+    }
+    
+    @IBAction func HomeButtonAction(_ sender: Any) {
+    
+        let homeStoryboard = AppStoryBoard.Home.instance
+        let homeVC = homeStoryboard.instantiateViewController(withIdentifier: "HomeVC")
+        self.present(homeVC, animated: true) {
+            
+        }
+        
+    }
+
+    @IBAction func settingsButtonAction(_ sender: Any) {
+        let settingsStoryBoard = AppStoryBoard.Settings.instance
+        let settingsVC = settingsStoryBoard.instantiateViewController(withIdentifier: "SettingsVC")
+        
+        
+    
+    }
+    
+
+
 
 }
 
